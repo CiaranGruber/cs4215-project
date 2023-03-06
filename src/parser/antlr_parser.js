3899af8aaf3eebfbc1c4e4021fe6e359c78f1446
Ciaran Gruber <ciaran.gruber@outlook.com>
2023-03-06 16:20:54 +0800

    Partially complete Antlr4 parser implementation

---
+++ src/parser/antlr_parser.js
@@ -0,0 +1,10 @@
+/**
+ * Antlr Parser
+ *
+ * Created for CS4215 term project
+ *
+ * By Ciaran Gruber and Jody Tang
+ */
+var message = "Are you there?";
+console.log(message);
+//# sourceMappingURL=antlr_parser.js.map
--- src/parser/antlr_parser.ts
+++ src/parser/antlr_parser.ts
@@ -6,6 +6,19 @@
  * By Ciaran Gruber and Jody Tang
  */
 
+import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+import {CLexer} from "./antlr_out/CLexer";
+import {CParser} from "./antlr_out/CParser";
+
+// Create the lexer and parser
+let inputStream = new ANTLRInputStream("text");
+let lexer = new CLexer(inputStream);
+let tokenStream = new CommonTokenStream(lexer);
+let parser = new CParser(tokenStream);
+
+// Parse the input, where `compilationUnit` is whatever entry point you defined
+let tree = parser.compilationUnit();
+
 let message: string = "Are you there?"
 
 console.log(message)
