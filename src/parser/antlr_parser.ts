9ec4e1cb5476f62820b9668bf55fab7125d92c34
Ciaran Gruber <ciaran.gruber@outlook.com>
2023-03-07 17:04:41 +0800

    Temporary Commit

--- src/parser/antlr_parser.ts
+++ src/parser/antlr_parser.ts
@@ -6,12 +6,15 @@
  * By Ciaran Gruber and Jody Tang
  */
 
+import { CompilationUnitContext } from "./antlr_gen/CParser";
 import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
-import {CLexer} from "./antlr_out/CLexer";
-import {CParser} from "./antlr_out/CParser";
+import {CLexer} from "./antlr_gen/CLexer";
+import {CParser} from "./antlr_gen/CParser";
+import {AbstractParseTreeVisitor} from "antlr4ts/tree";
+import {CVisitor} from "./antlr_gen/CVisitor";
 
 // Create the lexer and parser
-let inputStream = new ANTLRInputStream("text");
+let inputStream = new ANTLRInputStream("int a = 3;");
 let lexer = new CLexer(inputStream);
 let tokenStream = new CommonTokenStream(lexer);
 let parser = new CParser(tokenStream);
@@ -19,6 +22,24 @@ let parser = new CParser(tokenStream);
 // Parse the input, where `compilationUnit` is whatever entry point you defined
 let tree = parser.compilationUnit();
 
-let message: string = "Are you there?"
+// Extend the AbstractParseTreeVisitor to get default visitor behaviour
+class CountFunctionsVisitor extends AbstractParseTreeVisitor<number> implements CVisitor<number> {
 
-console.log(message)
+    defaultResult() {
+        return 0
+    }
+
+    aggregateResult(aggregate: number, nextResult: number) {
+        return aggregate + nextResult
+    }
+
+    visit(context: CompilationUnitContext): number {
+        return 1 + super.visitChildren(context)
+    }
+}
+
+// Create the visitor
+const countFunctionsVisitor = new CountFunctionsVisitor()
+
+// Use the visitor entry point
+countFunctionsVisitor.visit(tree)
